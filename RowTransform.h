132cc4116e733201340f9491076e77f270e1f778
Bharat Deleep Nair <bhtnair@gmail.com>
2024-09-26 01:25:12 +0530

    first commit

---
+++ RowTransform.h
@@ -0,0 +1,15 @@
+// Row Transformations
+#ifndef ROW_H
+#define ROW_H
+
+#include<vector>
+
+typedef  std::vector<float> intRow;
+
+intRow Rowdiv (intRow Row, float x);
+intRow Rowmultiply (intRow Row, float x);
+intRow RowsubRow (intRow Row1, intRow Row2);
+intRow RowadddRow (intRow Row1, intRow Row2);
+float roundTo4Decimals(float value);
+
+#endif
